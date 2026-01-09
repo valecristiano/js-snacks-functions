811e9fee539835651393aab22e72cc51b8343908
valecristiano <valeriamdcristiano@gmail.com>
2026-01-09 13:41:56 +0100

    iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,29 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e 
+restituisca nuovo array con le iniziali di ogni parola dell'array fornito */
 
-const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+let names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+console.table(names);
 
 // Dichiara la funzione qui.
-
+/**
+ *
+ * @param {Array} namesArray
+ * @returns {Array}
+ */
+function initialsArray(namesArray) {
+  const newArray = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    const currentName = namesArray[i];
+    const initial = currentName[0];
+    console.log("initial", initial);
+    newArray.push(initial);
+  }
+  return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const output = initialsArray(names);
+console.log(output);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
