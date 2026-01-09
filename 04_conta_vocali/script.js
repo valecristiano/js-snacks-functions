340a6f3df84c93d14cc849bf7fc6ae241585329d
valecristiano <valeriamdcristiano@gmail.com>
2026-01-09 14:31:34 +0100

    vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,22 +1,30 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = "javascript";
-console.log(word);
+const word = "compilazione";
 
 // Dichiara la funzione qui.
 function vowelsNumber(phrase) {
   phrase = phrase.split("");
-  console.log(phrase);
+  console.log("word array:", phrase);
+  const vowelsArray = [];
+  const vowels = ["a", "e", "i", "o", "u"];
+
   for (let i = 0; i < phrase.length; i++) {
     const currentLetter = phrase[i];
-    console.log(currentLetter);
-  }
 
-  return;
+    for (let i = 0; i < vowels.length; i++) {
+      const currentVowel = vowels[i];
+      if (currentLetter === currentVowel) {
+        vowelsArray.push(currentLetter);
+      }
+    }
+  }
+  return vowelsArray.length;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-word = vowelsNumber(word);
+const output = vowelsNumber(word);
+console.log(output);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
