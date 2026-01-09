867b92249cd6facd9127badd6d562e3614d085c6
valecristiano <valeriamdcristiano@gmail.com>
2026-01-09 13:54:51 +0100

    filtra iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,25 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+/* Scrivi una funzione che accetti un'array di stringhe e
+una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
-const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const names = ["Anna", "Luca", "Marco", "adele", "Laura", "Alessandra"];
+console.log(names);
 
 // Dichiara la funzione qui.
-
+function letterArray(stringArray, letter) {
+  const newArray = [];
+  for (i = 0; i < stringArray.length; i++) {
+    currentWord = stringArray[i];
+    initial = currentWord[0].toUpperCase();
+    if (initial === letter.toUpperCase()) {
+      newArray.push(currentWord);
+    }
+  }
+  return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const output = letterArray(names, "a");
+console.log(output);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
