996774739485ecb0e557768a498f63117d9569a2
valecristiano <valeriamdcristiano@gmail.com>
2026-01-09 14:45:20 +0100

    bonus start

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,28 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+// const name = 'Mario';
 
 // Dichiara la funzione qui.
+function time() {
+  const currentDate = new Date();
+  const currentHour = currentDate.getHours();
+  console.log("Ora attuale:", currentHour);
+  return currentHour;
+}
 
+function greet(name) {
+    if ()
+  console.log("Buongiorno", name);
+  console.log("Buonpomeriggio", name);
+  console.log("Buonasera", name);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const orario = time();
 
+const userName = greet("Mario");
+console.log(userName);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
