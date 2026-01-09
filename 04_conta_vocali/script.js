18aa398bc959f0ec6583332c4dc86f7a69df2c75
valecristiano <valeriamdcristiano@gmail.com>
2026-01-09 14:06:04 +0100

    vocali start

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,22 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
+console.log(word);
 
 // Dichiara la funzione qui.
+function vowelsNumber(phrase) {
+  phrase = phrase.split("");
+  console.log(phrase);
+  for (let i = 0; i < phrase.length; i++) {
+    const currentLetter = phrase[i];
+    console.log(currentLetter);
+  }
 
+  return;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+word = vowelsNumber(word);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
